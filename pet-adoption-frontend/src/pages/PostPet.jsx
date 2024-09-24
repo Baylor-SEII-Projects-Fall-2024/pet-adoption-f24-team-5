adf2b32cf3f5952116260fd48751155047c72f04
Sizboo <benny03.szabo@gmail.com>
2024-09-24 14:25:23 -0500

    blank page with welcome greeting

---
+++ pet-adoption-frontend/src/pages/PostPet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostPet = () => {
+    return (
+        <div>
+            <h1>Post a Pet for Adoption Here</h1>
+        </div>
+    )
+}
+
+export default PostPet
