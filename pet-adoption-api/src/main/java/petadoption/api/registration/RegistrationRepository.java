4a039865e64fc559c60077d0934ddd73bf6a9179
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-09-25 15:10:35 -0500

    Created RegisterUser, a frontend React page

--- pet-adoption-api/src/main/java/petadoption/api/registration/RegistrationRepository.java
+++ pet-adoption-api/src/main/java/petadoption/api/registration/RegistrationRepository.java
@@ -1,4 +1,9 @@
 package petadoption.api.registration;
 
-public class RegistrationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+
 }
---
+++ pet-adoption-frontend/src/pages/RegisterUser.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const RegisterUser = () => {
+    return (
+        <div>
+            <h1>Register a user here</h1>
+        </div>
+    )
+}
+export default RegisterUser
