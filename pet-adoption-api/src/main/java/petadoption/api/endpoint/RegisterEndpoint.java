58a0498e6647bfd3b0017cadd81f10c247325c81
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-09-25 15:10:35 -0500

    Created RegisterUser, a frontend React page

--- pet-adoption-api/src/main/java/petadoption/api/endpoint/RegisterEndpoint.java
+++ pet-adoption-api/src/main/java/petadoption/api/endpoint/RegisterEndpoint.java
@@ -3,11 +3,11 @@ package petadoption.api.endpoint;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import petadoption.api.registration.RegisterService;
+import petadoption.api.registration.RegistrationService;
 
 @Log4j2
 @RestController
 public class RegisterEndpoint {
     @Autowired
-    private RegisterService registerService;
+    private RegistrationService registrationService;
 }
