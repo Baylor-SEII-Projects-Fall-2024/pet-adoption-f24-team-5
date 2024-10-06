57196ea0bfeb596447d9197d82c6d10722786cb9
Sizboo <benny03.szabo@gmail.com>
2024-10-06 16:08:23 -0500

    stubbed initial hooks

--- pet-adoption-frontend/src/pages/PostPet.jsx
+++ pet-adoption-frontend/src/pages/PostPet.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 
 const PostPet = () => {
+    const [species, setSpecies] = React.useState('');
+    const [petName, setPetName] = React.useState('');
+    const [breed, setBreed] = React.useState('');
+    const [color, setColor] = React.useState('');
+    const [age, setAge] = React.useState('');
+    const [adoptionStatus, setStatus] = React.useState(false);
+    const [image, setImage] = React.useState('');
+    const [pets, setPets] = React.useState(
+        {name: "Pet name", image: "image of pet", description: "description of pets"}
+    );
+    const [postNewPet, setPostNewPet] = React.useState(false);
+
+
+
     return (
         <div>
             <h1>Post a Pet for Adoption Here</h1>
