41d4ed8d5e70797b25ea94cac9f4326dc300e986
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-10-07 08:33:35 -0500

    Updated event controller, event repository, and event service

--- pet-adoption-api/src/main/java/petadoption/api/Event/EventService.java
+++ pet-adoption-api/src/main/java/petadoption/api/Event/EventService.java
@@ -1,4 +1,42 @@
 package petadoption.api.Event;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import petadoption.api.user.AdoptionCenter.AdoptionCenter;
+import petadoption.api.user.AdoptionCenter.AdoptionCenterRepository;
+import petadoption.api.user.AdoptionCenter.CenterWorker;
+import petadoption.api.user.AdoptionCenter.CenterWorkerRepository;
+import petadoption.api.user.Owner.Owner;
+import petadoption.api.user.Owner.OwnerRepository;
+import petadoption.api.user.User;
+import petadoption.api.user.UserRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EventService {
+    @Autowired
+    private EventRepository eventRepository;
+
+    public Optional<Event> findEvent(Long event_id) {return eventRepository.findById(event_id);}
+
+    public Event saveEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    public void deleteEvent(Long event_id) {
+        eventRepository.deleteById(event_id);
+    }
+
+    public List<Event> findAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    public Long createEvent(Event event) throws IllegalArgumentException{
+        eventRepository.save(event);
+        return event.getEvent_id();
+    }
 }
+
