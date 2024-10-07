aa9dd1d9eb4a2e38182782a6ebf1db871d5bad7f
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-10-07 08:33:35 -0500

    Updated event controller, event repository, and event service

--- pet-adoption-api/src/main/java/petadoption/api/Event/EventController.java
+++ pet-adoption-api/src/main/java/petadoption/api/Event/EventController.java
@@ -1,4 +1,37 @@
 package petadoption.api.Event;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/users")
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class EventController {
+    @Autowired
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping
+    public List<Event> getEvent() {
+        return eventService.findAllEvents();
+    }
+
+    @PostMapping("/create/event")
+    public ResponseEntity<?> register(@RequestBody Event event) {
+        try{
+            Long id = eventService.createEvent(event);
+            return new ResponseEntity<>(id, HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+        }
+    }
+
 }
+
