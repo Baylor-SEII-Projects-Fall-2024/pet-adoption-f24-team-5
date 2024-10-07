a962f0ebdeea0af9f7a9396e47549f8b82525b28
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-10-07 07:58:06 -0500

    Added Event package as well as the following classes: Event, EventService

--- pet-adoption-api/src/main/java/petadoption/api/Event/EventService.java
+++ pet-adoption-api/src/main/java/petadoption/api/Event/EventService.java
@@ -1,32 +1,4 @@
 package petadoption.api.Event;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.Optional;
-
-@Service
 public class EventService {
-    @Autowired
-    private EventRepository eventRepository;
-
-    public Optional<Event> findEvent(Long event_id) {return eventRepository.findById(event_id);}
-
-    public Event saveEvent(Event event) {
-        return eventRepository.save(event);
-    }
-
-    public void deleteEvent(Long event_id) {
-        eventRepository.deleteById(event_id);
-    }
-
-    public List<Event> findAllEvents() {
-        return eventRepository.findAll();
-    }
-
-    public Long createEvent(Event event) throws IllegalArgumentException{
-        eventRepository.save(event);
-        return event.getEvent_id();
-    }
 }
-
