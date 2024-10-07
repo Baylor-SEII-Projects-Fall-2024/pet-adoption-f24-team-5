937c1433e2ba8aa452c344f83cffc1e8f6ca4a3d
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-10-07 08:34:19 -0500

    Updated event controller, event repository, and event service

--- pet-adoption-api/src/main/java/petadoption/api/Event/EventRepository.java
+++ pet-adoption-api/src/main/java/petadoption/api/Event/EventRepository.java
@@ -2,8 +2,6 @@ package petadoption.api.Event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface EventRepository extends JpaRepository<Event, Long> {
 
 }
