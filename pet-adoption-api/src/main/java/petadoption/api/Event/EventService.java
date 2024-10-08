25d29c9a34a179d49a3419e69e7b3299b2a1becd
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-10-08 14:14:36 -0500

    Updated EventService to remove functions already built in

--- pet-adoption-api/src/main/java/petadoption/api/Event/EventService.java
+++ pet-adoption-api/src/main/java/petadoption/api/Event/EventService.java
@@ -9,21 +9,9 @@ import java.util.Optional;
 public class EventService {
     @Autowired
     private EventRepository eventRepository;
-
-    public Optional<Event> findEvent(Long event_id) {return eventRepository.findById(event_id);}
-
     public Event saveEvent(Event event) {
         return eventRepository.save(event);
     }
-
-    public void deleteEvent(Long event_id) {
-        eventRepository.deleteById(event_id);
-    }
-
-    public List<Event> findAllEvents() {
-        return eventRepository.findAll();
-    }
-
     public Long createEvent(Event event) throws IllegalArgumentException{
         eventRepository.save(event);
         return event.getEvent_id();
