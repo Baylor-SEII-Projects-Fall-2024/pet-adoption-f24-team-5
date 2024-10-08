8b1beb543ab12ad3cef3c662a16950b85901ff04
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-10-08 15:48:36 -0500

    Updated EventConfig to not collide with commandLineRunner in User

--- pet-adoption-api/src/main/java/petadoption/api/Event/EventConfig.java
+++ pet-adoption-api/src/main/java/petadoption/api/Event/EventConfig.java
@@ -1,14 +1,28 @@
 package petadoption.api.Event;
 
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import petadoption.api.user.AdoptionCenter.CenterWorker;
+import petadoption.api.user.UserType;
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
 
 @Configuration
 public class EventConfig {
     @Bean
     CommandLineRunner eventCommandLineRunner(EventRepository eventRepository) {
-        return args -> {};
+        return args -> {
+            Event event1 = new Event(
+                    1L,
+                    new Date(),
+                    new Time(1,1,1),
+                    "This is the description"
+            );
+
+            eventRepository.saveAll(List.of(event1));
+        };
     }
 }
