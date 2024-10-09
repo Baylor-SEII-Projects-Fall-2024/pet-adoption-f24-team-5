fa46f5cde5148b34b64f4056cf697f13b2b7284d
Andrew Parks <andrewparks@Andrews-MacBook-Pro.local>
2024-10-09 10:50:02 -0500

    removed ability to edit email, and replaced with editing first and last name

--- pet-adoption-frontend/src/pages/settings.jsx
+++ pet-adoption-frontend/src/pages/settings.jsx
@@ -5,15 +5,17 @@ import styles from '@/styles/Home.module.css';
 
 export default function HomePage() {
   const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [emailLabel, setEmailLabel] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [firstNameLabel, setFirstNameLabel] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [lastNameLabel, setLastNameLabel] = useState('');
   const [password, setPassword] = useState('');
   const [passwordLabel, setPasswordLabel] = useState('');
   const [oldPassword, setOldPassword] = useState('');
   const [oldPasswordLabel, setOldPasswordLabel] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [phoneNumberLabel, setPhoneNumberLabel] = useState('');
-  const [invalidEmail, setInvalidEmail] = useState(false);
+  const [invalidPhoneNumber, setInvalidPhoneNumber] = useState(false);
 
   // Uncomment and use when ready
   /*useEffect(() => {
@@ -32,14 +34,13 @@ export default function HomePage() {
     fetchUsername();
   }, []);*/
 
-  const handleEmailChange = () => {
-    if (!emailLabel.includes('@')) {
-      setInvalidEmail(true);
-    } else {
-      setInvalidEmail(false);
-    }
-    setEmail(emailLabel);
+  const handleFirstNameChange = () => {
+    setFirstName(firstNameLabel);
   };
+
+  const handleLastNameChange = () => {
+    setLastName(lastNameLabel);
+  }
 
   const handlePasswordChange = () => {
     setPassword(passwordLabel);
@@ -48,6 +49,12 @@ export default function HomePage() {
   };
 
   const handlePhoneNumberChange = () => {
+    if (!phoneNumberLabel.length < 9){
+      setInvalidPhoneNumber(true);
+    }
+    else{
+      setInvalidPhoneNumber(false);
+    }
     setPhoneNumber(phoneNumberLabel);
   };
 
@@ -66,20 +73,28 @@ export default function HomePage() {
           </Card>
           <Paper sx={{ width: 600, height: 50 }} elevation={4}>
             <Stack spacing={1} direction="row" alignItems='center'>
-              <Typography variant='h5'>Email</Typography>
+              <Typography variant='h5'>First Name</Typography>
               <TextField
-                label="Email"
+                label="First Name"
                 align='center'
-                value={emailLabel}
-                onChange={(e) => setEmailLabel(e.target.value)}
-                InputProps={{ style: { height: '40px', width: '450px' } }}
+                value={firstNameLabel}
+                onChange={(e) => setFirstNameLabel(e.target.value)}
+                InputProps={{ style: { height: '40px', width: '400px' } }}
               />
-              {invalidEmail && (
-                <Typography color="error" variant="body2">
-                  Please enter a valid email address.
-                </Typography>
-              )}
-              <Button onClick={handleEmailChange}>Change</Button>
+              <Button onClick={handleFirstNameChange}>Change</Button>
+            </Stack>
+          </Paper>
+          <Paper sx={{ width: 600, height: 50 }} elevation={4}>
+            <Stack spacing={1} direction="row" alignItems='center'>
+              <Typography variant='h5'>Last Name</Typography>
+              <TextField
+                label="Last Name"
+                align='center'
+                value={lastNameLabel}
+                onChange={(e) => setLastNameLabel(e.target.value)}
+                InputProps={{ style: { height: '40px', width: '400px' } }}
+              />
+              <Button onClick={handleLastNameChange}>Change</Button>
             </Stack>
           </Paper>
           <Paper sx={{ width: 600, height: 50 }} elevation={4}>
@@ -90,8 +105,13 @@ export default function HomePage() {
                 align='center'
                 value={phoneNumberLabel}
                 onChange={(e) => setPhoneNumberLabel(e.target.value)}
-                InputProps={{ style: { height: '40px', width: '350px' } }}
+                InputProps={{ style: { height: '40px', width: '300px' } }}
               />
+              {invalidPhoneNumber && (
+                <Typography color="error" variant="body2">
+                  Please enter a valid phone number.
+                </Typography>
+              )}
               <Button onClick={handlePhoneNumberChange}>Change</Button>
             </Stack>
           </Paper>
