eebccc9d4e1dde94bd4931ee4a36f04173afacf5
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-10-09 15:58:27 -0500

    Bug fixing to get events to display correctly

--- pet-adoption-api/src/main/java/petadoption/api/Event/EventService.java
+++ pet-adoption-api/src/main/java/petadoption/api/Event/EventService.java
@@ -4,12 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class EventService {
