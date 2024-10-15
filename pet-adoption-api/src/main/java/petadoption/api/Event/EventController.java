6dedad54de3a93a2b0565736be3e9087aeac4f8c
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-10-15 16:38:52 -0500

    Warning correcting

--- pet-adoption-api/src/main/java/petadoption/api/Event/EventController.java
+++ pet-adoption-api/src/main/java/petadoption/api/Event/EventController.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
