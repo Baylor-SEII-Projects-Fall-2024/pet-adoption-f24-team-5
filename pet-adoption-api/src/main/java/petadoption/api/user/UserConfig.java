68bf25d2a22faf18ee4357f0ca9cc033b812f810
CremDeLaPP <peter.whitcomb727@gmail.com>
2024-10-15 10:36:55 -0500

    Fixed config file to not include duplicates

--- pet-adoption-api/src/main/java/petadoption/api/user/UserConfig.java
+++ pet-adoption-api/src/main/java/petadoption/api/user/UserConfig.java
@@ -22,6 +22,8 @@ public class UserConfig {
     CommandLineRunner commandLineRunner(UserService userService) {
         return args -> {
 
+            userService.initialize();
+
 
         };
     }
