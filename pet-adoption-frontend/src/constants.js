c42873db181828c918a825db65ecb7a1b7951d1d
CremDeLaPP <peter.whitcomb727@gmail.com>
2024-10-16 11:48:05 -0500

    Changed main to be able to configure to gcp really easily

--- pet-adoption-frontend/src/constants.js
+++ pet-adoption-frontend/src/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://34.27.150.181";
-export const FRONTEND_URL = "http://34.173.164.84:3000";
+export const API_URL = "http://localhost:8080";
+export const FRONTEND_URL = "http://localhost:3000";
