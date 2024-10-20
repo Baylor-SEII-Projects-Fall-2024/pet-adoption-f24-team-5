e05a5830c289fb42a1ce1d47337590acbd54d4d1
Sizboo <benny03.szabo@gmail.com>
2024-10-20 14:25:00 -0500

    changed backend for handling images

--- pet-adoption-api/src/main/java/petadoption/api/images/ImageController.java
+++ pet-adoption-api/src/main/java/petadoption/api/images/ImageController.java
@@ -1,11 +1,15 @@
 package petadoption.api.images;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.Map;
 
 @RequestMapping("api/images")
 @RestController
@@ -19,14 +23,23 @@ public class ImageController {
     public ImageController(ImageService imageService) { this.imageService = imageService; }
 
     @PostMapping("/upload")
-    public String uploadImage(@RequestParam("file") MultipartFile file) throws IOException, FileNotFoundException {
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) {
         System.out.println(file.getOriginalFilename());
-        return imageService.saveImage(UPLOAD_DIRECTORY, file);
+
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(imageService.saveImage(UPLOAD_DIRECTORY, file));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An unexpected error occurred, please try again later");
+        }
     }
 
     @GetMapping("/{imageName}")
-    public byte[] getImage(@PathVariable String imageName) throws IOException {
-        return imageService.getImage(UPLOAD_DIRECTORY, imageName);
+    public ResponseEntity<Map.Entry<String, String>> getImage(@PathVariable String imageName) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(imageService.getImage(UPLOAD_DIRECTORY, imageName));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new AbstractMap.SimpleEntry<>("Error", e.getMessage()));
+        }
     }
 
 }
