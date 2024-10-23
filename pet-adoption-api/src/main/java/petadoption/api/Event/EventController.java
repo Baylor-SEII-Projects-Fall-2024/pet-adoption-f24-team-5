2087eaa9a68b3942e282dec7261a58a8d0642aee
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-10-22 20:19:07 -0500

    Removed fluff

--- pet-adoption-api/src/main/java/petadoption/api/Event/EventController.java
+++ pet-adoption-api/src/main/java/petadoption/api/Event/EventController.java
@@ -5,9 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @RequestMapping("/api/events")
