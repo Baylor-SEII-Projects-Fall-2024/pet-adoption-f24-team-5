2bf2fe932d46539ee3f97a3863f374d4f49f2b97
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-10-22 19:42:41 -0500

    removed fluff

--- pet-adoption-api/src/main/java/petadoption/api/Event/EventRepository.java
+++ pet-adoption-api/src/main/java/petadoption/api/Event/EventRepository.java
@@ -1,9 +1,6 @@
 package petadoption.api.Event;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
