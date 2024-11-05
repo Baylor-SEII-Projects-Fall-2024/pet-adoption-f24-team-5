1bcddb780b9701fee117706ffb49117f6326cc40
Sizboo <benny03.szabo@gmail.com>
2024-11-05 14:54:55 -0600

    blur content for card working

--- pet-adoption-frontend/src/components/PetCard.jsx
+++ pet-adoption-frontend/src/components/PetCard.jsx
@@ -1,16 +1,51 @@
-import { Card, CardContent, Typography, CircularProgress, Box } from "@mui/material";
+import {Card, CardContent, Typography, CircularProgress, Box, styled} from "@mui/material";
 import ImageComponent from "@/components/ImageComponent";
 import { useState } from "react";
+import {theme} from "@/utils/theme";
+
+const StyledCard = styled(Card)(({ theme, isHovered }) => ({
+    position: 'relative',
+    overflow: 'hidden',
+    transition: '0.3s',
+}));
+
+
+const BlurredContent = styled(Box)(({ isHovered }) => ({
+    transition: 'filter 0.3s',
+    ...(isHovered && {
+        filter: 'blur(2px)',
+    }),
+}));
+
+const HoverOverlay = styled(Box)(({ theme }) => ({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    color: 'white',
+    opacity: 0,
+    transition: 'opacity 0.3s',
+    '&.hovered': {
+        opacity: 1,
+    },
+}));
 
 const PetCard = ({ pet, onClick }) => {
     const [loading, setLoading] = useState(true);
+    const [isHovered, setIsHovered] = useState(false);
 
     const handleImageLoad = () => {
         setLoading(false);
     };
 
     return (
-        <Card
+        <StyledCard
             onClick={onClick}
             sx={{
                 width: "48%",
@@ -20,40 +55,47 @@ const PetCard = ({ pet, onClick }) => {
             }}
             elevation={2}
             key={pet.petName}
+            isHovered={isHovered}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
         >
-            <CardContent>
-                <Box
-                    position="relative"
-                    width="100%"
-                    maxWidth="200px"
-                    height="auto"
-                    margin="0 15px 15px 0"
-                >
-                    {loading && (
-                        <Box
-                            display="flex"
-                            alignItems="center"
-                            justifyContent="center"
-                            position="absolute"
-                            top={0}
-                            left={0}
-                            width="100%"
-                            height="100%"
-                            bgcolor="rgba(255, 255, 255, 0.6)"
-                            zIndex={1}
-                        >
-                            <CircularProgress />
-                        </Box>
-                    )}
-
-                    <ImageComponent
-                        imageName={pet.imageName}
+            <BlurredContent isHovered={isHovered}>
+                <CardContent>
+                    <Box
+                        position="relative"
                         width="100%"
                         maxWidth="200px"
                         height="auto"
-                        onLoad={handleImageLoad} // Passing onLoad to ImageComponent.
-                    />
-                </Box>
+                        margin="0 15px 15px 0"
+                    >
+                        {loading && (
+                            <Box
+                                display="flex"
+                                alignItems="center"
+                                justifyContent="center"
+                                position="absolute"
+                                top={0}
+                                left={0}
+                                width="100%"
+                                height="100%"
+                                bgcolor="rgba(255, 255, 255, 0.6)"
+                                zIndex={1}
+                            >
+                                <CircularProgress />
+                            </Box>
+                        )}
+
+                        <ImageComponent
+                            imageName={pet.imageName}
+                            width="100%"
+                            maxWidth="200px"
+                            height="auto"
+                            onLoad={handleImageLoad} // Passing onLoad to ImageComponent.
+                        />
+                    </Box>
+                </CardContent>
+            </BlurredContent>
+            <HoverOverlay className={isHovered ? 'hovered' : ''}>
 
                 <Typography variant="h4" align="left">
                     {pet.petName}
@@ -73,8 +115,8 @@ const PetCard = ({ pet, onClick }) => {
                 <Typography variant="body2" align="left">
                     Description: {pet.description}
                 </Typography>
-            </CardContent>
-        </Card>
+            </HoverOverlay>
+        </StyledCard>
     );
 };
 
--- pet-adoption-frontend/src/constants.js
+++ pet-adoption-frontend/src/constants.js
@@ -1,11 +1,12 @@
-
+/*
 export const API_URL = "http://34.66.92.107";
 export const FRONTEND_URL = "http://34.173.164.84:3000";
 
+ */
 
 
-/*
+
 export const API_URL = "http://localhost:8080";
 export const FRONTEND_URL = "http://localhost:3000";
 
-*/
+
