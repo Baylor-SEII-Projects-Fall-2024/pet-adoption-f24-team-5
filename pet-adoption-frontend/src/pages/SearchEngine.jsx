8f3fb87f24362170bc5bbbb3d9bfafbbb313ae74
CremDeLaPP <peter.whitcomb727@gmail.com>
2024-11-13 14:33:40 -0600

    added pet injects and welcome message explaining how it works

--- pet-adoption-frontend/src/pages/SearchEngine.jsx
+++ pet-adoption-frontend/src/pages/SearchEngine.jsx
@@ -14,6 +14,7 @@ const SearchEngine = () => {
     const [showSavedPets, setShowSavedPets] = React.useState(false);
     const [savedPets, setSavedPets] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [showWelcomeModal, setShowWelcomeModal] = useState(false);
 
     const handleSearch = async () => {
         setPets([]);
@@ -36,6 +37,11 @@ const SearchEngine = () => {
 
     useEffect(() => {
         handleSearch();
+        setShowWelcomeModal(true); // Show welcome modal on first load
+    }, []);
+
+    useEffect(() => {
+        handleSearch();
     }, []);
 
     const handleOpenSavedPets = () => {
@@ -45,6 +51,10 @@ const SearchEngine = () => {
 
     const handleCloseSavedPets = () => {
         setShowSavedPets(false);
+    }
+
+    const handleCloseWelcomeModal = () => {
+        setShowWelcomeModal(false);
     }
 
     const fetchSavedPets = useCallback(async () => {
@@ -84,6 +94,53 @@ const SearchEngine = () => {
                     background: 'linear-gradient(135deg, #edf2f7, #dce2e9)',
                 }}
             >
+                <Modal
+                    open={showWelcomeModal}
+                    onClose={handleCloseWelcomeModal}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Box
+                        sx={{
+                            width: '400px',
+                            backgroundColor: 'white',
+                            borderRadius: 4,
+                            boxShadow: 24,
+                            p: 4,
+                            textAlign: 'center'
+                        }}
+                    >
+                        <Typography variant="h4" sx={{ mb: 2 }}>
+                            Welcome to the Pet Search Engine!
+                        </Typography>
+                        <Typography variant="body1" sx={{ mb: 4 }}>
+                            Find your perfect pet match by exploring the recommendations below, here is how it works:
+                        </Typography>
+                        <Typography variant="body2" sx={{ mb: 4 }}>
+                            1. If this is your first time the first three rounds will pe preliminary meaning it is gauging your initial preferences by showing random pets
+                        </Typography>
+                        <Typography variant="body2" sx={{ mb: 4 }}>
+                            2. If you like a pet it will take you preferences into account, and suggest more pets similar to that one
+                        </Typography>
+
+                        <Button
+                            onClick={handleCloseWelcomeModal}
+                            variant="contained"
+                            sx={{
+                                background: 'linear-gradient(90deg, #43cea2, #246e56)',
+                                color: 'white',
+                                '&:hover': {
+                                    background: 'linear-gradient(90deg, #246e56, #43cea2)',
+                                },
+                            }}
+                        >
+                            Get Started
+                        </Button>
+                    </Box>
+                </Modal>
                 {!showSavedPets && (
                     <Grid container spacing={2} sx={{ height: '100vh', width: '100vw', padding: 2 }}>
 
