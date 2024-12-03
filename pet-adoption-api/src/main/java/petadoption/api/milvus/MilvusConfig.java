4c9de07a4555079a3ae57b56c67e265f0af33e50
Sizboo <benny03.szabo@gmail.com>
2024-12-03 13:09:53 -0600

    further testing

--- pet-adoption-api/src/main/java/petadoption/api/milvus/MilvusConfig.java
+++ pet-adoption-api/src/main/java/petadoption/api/milvus/MilvusConfig.java
@@ -4,6 +4,7 @@ import io.milvus.client.MilvusClient;
 import io.milvus.v2.client.ConnectConfig;
 import io.milvus.v2.client.MilvusClientV2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
