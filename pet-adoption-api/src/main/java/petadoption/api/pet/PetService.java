d02239b529a572f3ed4daf0350eb00d5b21b8c04
MitchThompson2001 <pulls-tinker-0w@icloud.com>
2024-12-05 14:10:14 -0600

    adopt a pet route for backend

--- pet-adoption-api/src/main/java/petadoption/api/pet/PetService.java
+++ pet-adoption-api/src/main/java/petadoption/api/pet/PetService.java
@@ -2,8 +2,10 @@ package petadoption.api.pet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import petadoption.api.user.AdoptionCenter.AdoptionCenter;
+import petadoption.api.user.Owner.Owner;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +24,16 @@ public class PetService {
         pet.setAdoptionCenter(adoptionCenter);
         return petRepository.save(pet);
 
+    }
+
+    public void adoptPet(Pet pet, Owner owner) {
+        petRepository.findByPetId(pet.getPetId()).map(tempPet -> {
+            tempPet.setAdoptionStatus(true);
+            tempPet.setPetOwner(owner);
+            tempPet.setOwner(owner);
+            Pet savedPet = petRepository.save(tempPet);
+            return ResponseEntity.ok(savedPet);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     public List<Pet> getPetByAdoptionCenter(AdoptionCenter adoptionCenter) {
