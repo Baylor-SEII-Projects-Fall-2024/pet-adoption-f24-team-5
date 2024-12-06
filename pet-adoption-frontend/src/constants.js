e2578b21e8674e885788bac4d21d66be8bed59ac
JacksonHenryBotDojo <jackson@botdojo.com>
2024-12-06 16:39:55 -0600

    lol

--- pet-adoption-frontend/src/constants.js
+++ pet-adoption-frontend/src/constants.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://api.dogpilesolutions.com";
-// export const FRONTEND_URL = "http://34.72.46.121";
+export const FRONTEND_URL = "http://34.72.46.121";
 
 //export const API_URL = "http://localhost:8080";
-export const FRONTEND_URL = "http://localhost:3000";
+// export const FRONTEND_URL = "http://localhost:3000";
